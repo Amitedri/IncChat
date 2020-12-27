f4e2fa371bbc2770fb1f34a6d65721b08548c7a8
Amitedri <amitedri778@gmail.com>
2020-12-27 19:38:23 +0200

    updates in logic, fron, backend improvements

--- src/server/Utils.js
+++ src/server/Utils.js
@@ -1,28 +1,45 @@
-exports.generateMessage = (username, text) => {
-    return {
-        username: username,
-        message: text,
-        date: Date.now(),
-    };
+exports.checkIfUserExist = (usersList, user) => {
+    const { username } = user;
+    usersList.forEach((element) => {
+        if (element.username === username) {
+            console.log(element.username === username);
+            return true;
+        }
+        return false;
+    });
+};
+exports.setUsersUpdate = async (usersList, user, io) => {
+    try {
+        let isDouble = false;
+        const roomArray = usersList[user.room];
+        const { username } = user;
+        for (const activUser of roomArray) {
+            if (activUser.username === username) {
+                isDouble = true;
+                return roomArray;
+            }
+        }
+        if (!isDouble) {
+            roomArray.push(user);
+            return roomArray;
+        }
+    } catch (err) {
+        if (err) {
+            console.log(err);
+        }
+    }
 };
 
-exports.addUserToList = (user, array) => {
-    return array[user.room].push(user);
-};
-exports.onlineSockets = (value, io, usersInRooms) => {
+exports.onlineSockets = async (value, io, usersInRooms) => {
     const onlineSockets = Object.entries(io.sockets.adapter.rooms);
+    console.log(onlineSockets);
     for (const [key, mongo] of onlineSockets) {
         for (const singleUser of usersInRooms[value.room]) {
             if (singleUser) {
                 if (singleUser.id !== key) {
-                    const elemToRemove = usersInRooms[value.room].indexOf(
-                        singleUser
-                    );
-                    const updatedRoomUsers = usersInRooms[value.room].splice(
-                        elemToRemove,
-                        1
-                    );
-                    io.emit('usersListUpdate', updatedRoomUsers).to(value.room);
+                    const elemToRemove = usersInRooms[value.room].indexOf(singleUser);
+                    const updatedRoomUsers = usersInRooms[value.room].splice(elemToRemove, 1);
+                    io.to(value.room).emit('usersListUpdate', usersInRooms[value.room]);
                 }
             }
         }
--- src/server/server.js
+++ src/server/server.js
@@ -1,63 +1,60 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const connectToDB = require('../DB/dbConnection');
+const encrypt = require('socket.io-encrypt');
+require('dotenv').config();
 const cors = require('cors');
-const {addUserToList,onlineSockets,generateMessage} = require('./Utils')
-const port = 5000;
+const { setUsersUpdate, onlineSockets, generateMessage } = require('./Utils');
+const port = process.env.PORT || port;
 
 const app = express();
+//
 app.use(cors());
 
 //using http module to create a server
 const httpServer = http.createServer(app);
-
+//init socket
 const io = socketIo(httpServer);
-
+//room options
 let usersInRooms = {
     Javascript: [],
     Pentest: [],
     Python: [],
 };
-let disconnectSocket;
-
-io.on('connect', (socket) => {
+//socket logic
+io.on('connection', (socket) => {
     //request info of client
     socket.emit('newLogin');
-
-    socket.on('getUserData', (user) => {
-        //add client to users array
-        addUserToList(user, usersInRooms);
-        io.emit('usersListUpdate', usersInRooms[user.room]).to(user.room);
+    socket.on('getUserData', async (user) => {
         // adding user to room and emitting event with a string
         socket.join(user.room, function (err) {
             if (err) {
+                //logger implemntation will go here
                 console.log(err);
                 return;
             }
+            const welcomeString = `${user.username} has joined room ${user.room}!`;
+            return io.to(user.room).emit('messageFromServer', { username: 'Admin', message: welcomeString });
         });
-        //annouce in room
-        const welcomeString = `${user.username} has joined room ${user.room}!`;
+        //add client to users array
+        const updateUsers = await setUsersUpdate(usersInRooms, user, io);
+        // user.secret !== null || user.secret !== undefined ? io.use(encrypt(user.secret)) : null;
 
-        return io
-            .to(user.room)
-            .emit('messageFromServer', generateMessage('Admin', welcomeString));
+        return io.to(user.room).emit('usersListUpdate', updateUsers);
     });
-    
+
     socket.on('message', (message) => {
         io.to(message.room).emit('messageFromServer', message);
     });
 
-    socket.on('disconnect', (value) => {
-        io.emit('ping');
+    socket.on('disconnect', () => {
+        io.emit('userOut');
     });
-    socket.on('requestUsersUpdate', (value) => {
-        onlineSockets(value,io,usersInRooms);
+    socket.on('requestUsersUpdate', async (value) => {
+        const isFilterd = await onlineSockets(value, io, usersInRooms);
     });
 });
-
+//listener
 httpServer.listen(port, () => {
-    console.log('server on');
-    // connectToDB();
-    //IMPLEMENT DB CONNECTION
+    console.log(`server running on port ${port}`);
 });
