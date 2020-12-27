7dbb1fd7e7dd34c6f0e7bb6e83569552ea05b75e
Amitedri <amitedri778@gmail.com>
2020-12-27 19:42:18 +0200

    update in all areas-back and front

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,44 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Users.css';
-let users, userObj;
-let usersToMap;
-const Users = (props) => {
-    const usersRef = useRef(null);
-
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        setUsers((prevState) => {
-            console.log(users);
-
-            return props.users;
-        });
-        // const appendChild = (text) => {
-        //     const elem = document.createElement('div');
-        //     elem.classList.add('userContainer');
-        //     const valueElem = document.createElement('div');
-        //     valueElem.classList.add('username');
-        //     valueElem.textContent = text;
-        //     elem.append(valueElem);
-        //     return usersRef.current.append(elem);
-        // };
-        // if (users.length >= 1) {
-        //     var count = 0;
-        //     users.map((user) => {
-        //         appendChild(user.username);
-        //     });
-        // }
-    }, [props.users, window.location]);
-
-    const maybe = () => {};
+import { mapUsers } from '../ChatRoom/chatUtils';
+const Users = React.memo(({ usersList,color }) => {
     return (
         <div className="usersContainer">
-            <p>chats</p>
-            <div className="usersOutsideContainer">
-                <div ref={usersRef} className="usersInnerContainer" />
-            </div>
+            <div className="onlineUsersContainer">{mapUsers(usersList,color)}</div>
         </div>
     );
-};
+});
 
 export default Users;
--- src/index.js
+++ src/index.js
@@ -7,21 +7,19 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import particlesConfig from './particlesConfig.json';
 
 const App = () => {
-
-//setting particles effect
+    //setting particles effect
     useEffect(() => {
         const particlesJS = window.particlesJS;
         particlesJS('particles-js', particlesConfig);
     }, [window.location]);
 
-
     return (
         <div>
             <div id="particles-js">
-            <Switch>
-                <Route exact path="/chat" component={ChatRoom} />
-                <Route exat path="/" component={Join} />
-            </Switch>
+                <Switch>
+                    <Route exact path="/chat" component={ChatRoom} />
+                    <Route exat path="/" component={Join} />
+                </Switch>
             </div>
         </div>
     );
